7bb112474add6d8264494b73ea432473e6f146e0
Lou Bezzina <lou.bezzina@gmail.com>
2022-04-14 09:44:02 +0200

    Size-relative formTextFieldRow

--- lib/screens/widgets/FormTextFieldRow.dart
+++ lib/screens/widgets/FormTextFieldRow.dart
@@ -15,8 +15,8 @@ class FormTextFieldRow extends StatelessWidget {
         crossAxisAlignment: CrossAxisAlignment.center,
         mainAxisAlignment: MainAxisAlignment.spaceBetween,
         children: [
-          SizedBox(height:20, width: 150,child: Text(text.toUpperCase(),style: Styles.labelText, textAlign: TextAlign.center)),
-          SizedBox(height: 40, width: 150,child:TextField( decoration: InputDecoration(
+          SizedBox(height: 20, width: MediaQuery.of(context).size.width*3/10, child: Text(text.toUpperCase(), style: Styles.labelText, textAlign: TextAlign.center)),
+          SizedBox(height: 40, width: MediaQuery.of(context).size.width*45/100, child: TextField( decoration: InputDecoration(
               fillColor: Styles.greyedColor,
               filled: true,
               focusColor: Styles.accentColor,
