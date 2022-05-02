a1c59725020efc8d147dcfab0ebe5cb900a16fb5
scurila <stefania_curila@yahoo.ro>
2022-05-02 21:59:00 +0200

    Community screen done

---
+++ lib/screens/communaute/widgets/DefiCard.dart
@@ -0,0 +1,42 @@
+import 'package:artivity_front/screens/widgets/ReusableCard.dart';
+import 'package:flutter/material.dart';
+
+import '../../../theme/constants.dart';
+import '../../../theme/style.dart';
+
+class DefiCard extends StatelessWidget {
+  DefiCard({Key? key, required this.title, required this.imgUrl}) : super(key: key);
+  String title;
+  String imgUrl;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.only(top: 5, bottom: 5, left: 5, right: 5),
+      height: 170,
+      width: MediaQuery.of(context).size.width/2 - 35,
+      child: ReusableCard(
+        borderRadius: 9,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            SizedBox(
+              width: 160,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Image.asset(imgUrl, height: 95,),
+                ],
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(bottom: 5),
+              child: Text(title, style: Styles.challengeTitle, textAlign: TextAlign.left,),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
