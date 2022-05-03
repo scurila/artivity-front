f742c9758c0d82445a3e3cb0be1b890ea26dd1bd
Lou Bezzina <lou.bezzina@gmail.com>
2022-05-03 11:50:26 +0200

    CreationDefi1

--- lib/screens/creationDefi/widgets/TypeDefiCard.dart
+++ lib/screens/creationDefi/widgets/TypeDefiCard.dart
@@ -1,22 +1,25 @@
 import 'package:artivity_front/screens/widgets/ReusableCard.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 import '../../../theme/style.dart';
 
 class TypeDefiCard extends StatelessWidget {
-  const TypeDefiCard({Key? key, required this.title, required this.description, required this.imgUrl}) : super(key: key);
+  const TypeDefiCard({Key? key, required this.title, required this.description, required this.challengeType}) : super(key: key);
   final String title;
   final String description;
-  final String imgUrl;
+  final String challengeType;
 
   @override
   Widget build(BuildContext context) {
     return Container(
       margin: const EdgeInsets.only(top: 2, bottom: 2, left: 2, right: 2),
       width: MediaQuery.of(context).size.width/2 - 14,
+      height: MediaQuery.of(context).size.height*35/100,
       child: ReusableCard(
         borderRadius: 9,
         child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
             SizedBox(
@@ -25,12 +28,12 @@ class TypeDefiCard extends StatelessWidget {
                 mainAxisAlignment: MainAxisAlignment.center,
                 crossAxisAlignment: CrossAxisAlignment.center,
                 children: [
-                  Image.asset(imgUrl, height: 95,),
+                  SvgPicture.asset(Styles.getChallengeTypePicture(challengeType), width: MediaQuery.of(context).size.width*35/100,),
                 ],
               ),
             ),
             Padding(
-              padding: const EdgeInsets.only(bottom: 24),
+              padding: const EdgeInsets.only(bottom: 5),
               child: Text(title, style: Styles.challengeTitle, textAlign: TextAlign.left,),
             ),
             Text(description)
