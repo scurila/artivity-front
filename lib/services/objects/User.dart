05f9a1face25d7c5c184c490859a2dcd8000c671
Romain G <3607914-romain.galle@users.noreply.gitlab.com>
2022-05-04 22:41:39 +0200

    Added friends list service and updated friends screen

--- lib/services/objects/User.dart
+++ lib/services/objects/User.dart
@@ -5,14 +5,21 @@ import '../../theme/constants.dart';
 class  User {
   final int id;
   final String pseudo;
+  final String? bio;
 
   const User({
     required this.id,
     required this.pseudo,
-
+    this.bio
   });
 
-
+  factory User.fromJson(Map<String, dynamic> json) {
+    return User(
+      id: json['id'],
+      pseudo: json['pseudo'],
+      bio: json["bio"],
+    );
+  }
 }
 
 
